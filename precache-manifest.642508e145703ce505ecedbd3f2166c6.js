0f3078f9fa03138aa373513481b4c2effb538ca7
Betty <s9654025@gmail.com>
2019-09-05 10:47:29 +0800

    Updates

---
+++ precache-manifest.642508e145703ce505ecedbd3f2166c6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36885530d52a993daab82cfcb45df43d",
+    "url": "/cloud-drive/index.html"
+  },
+  {
+    "revision": "4bbd2f2ee8fd83659309",
+    "url": "/cloud-drive/static/css/main.f6cd1613.chunk.css"
+  },
+  {
+    "revision": "54f5b850bbc5b1e4d4b4",
+    "url": "/cloud-drive/static/js/2.2f9a3ee5.chunk.js"
+  },
+  {
+    "revision": "4bbd2f2ee8fd83659309",
+    "url": "/cloud-drive/static/js/main.364f3910.chunk.js"
+  },
+  {
+    "revision": "017695f8b003c1300fa2",
+    "url": "/cloud-drive/static/js/runtime~main.33c92508.js"
+  },
+  {
+    "revision": "8cfc565e1d153da9e2a9404cf36ed0fa",
+    "url": "/cloud-drive/static/media/add-folder.8cfc565e.svg"
+  },
+  {
+    "revision": "52ef67e51fe3117c11f80f63004ee40f",
+    "url": "/cloud-drive/static/media/cloud.52ef67e5.svg"
+  },
+  {
+    "revision": "bde6db5eb855429b7fe0a82307562be5",
+    "url": "/cloud-drive/static/media/doc.bde6db5e.svg"
+  },
+  {
+    "revision": "0ba4cd0c28e578f15375cf1509e1f952",
+    "url": "/cloud-drive/static/media/folder.0ba4cd0c.svg"
+  },
+  {
+    "revision": "cfdd3a41490eb65963f3be8078c5dd66",
+    "url": "/cloud-drive/static/media/garbage.cfdd3a41.svg"
+  },
+  {
+    "revision": "89a38b12d72dc895717e8dbf3722bc6d",
+    "url": "/cloud-drive/static/media/img.89a38b12.svg"
+  },
+  {
+    "revision": "ee43af90ebf678fb99882d020a79278d",
+    "url": "/cloud-drive/static/media/mp4.ee43af90.svg"
+  },
+  {
+    "revision": "cf5d0d359203e33463306f87212f04d4",
+    "url": "/cloud-drive/static/media/my-folder.cf5d0d35.svg"
+  },
+  {
+    "revision": "ee5ba3426be31e933f32c7c3e78ad023",
+    "url": "/cloud-drive/static/media/pdf.ee5ba342.svg"
+  },
+  {
+    "revision": "feed0a56518bdc7742976be25ec1c2d3",
+    "url": "/cloud-drive/static/media/search.feed0a56.svg"
+  },
+  {
+    "revision": "23dbd9d42e0c0ecd343e8cd186b62aa5",
+    "url": "/cloud-drive/static/media/sharing-content.23dbd9d4.svg"
+  },
+  {
+    "revision": "e1950d2eaad73369a296fedf361b3012",
+    "url": "/cloud-drive/static/media/star.e1950d2e.svg"
+  },
+  {
+    "revision": "60654512f6f28fd9a093a0494acd8b3a",
+    "url": "/cloud-drive/static/media/upload-file.60654512.svg"
+  },
+  {
+    "revision": "759fdd74960ce931782f91de6f6eea11",
+    "url": "/cloud-drive/static/media/upload-folder.759fdd74.svg"
+  },
+  {
+    "revision": "422b579b230d282027f484a90673a5e4",
+    "url": "/cloud-drive/static/media/upload-white.422b579b.svg"
+  },
+  {
+    "revision": "b253394960b72f26c79ee0ac699b615f",
+    "url": "/cloud-drive/static/media/upload.b2533949.svg"
+  },
+  {
+    "revision": "4d8aa304cb7f7bfda97d77cb66c2c51c",
+    "url": "/cloud-drive/static/media/user.4d8aa304.png"
+  }
+]);
